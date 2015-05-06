7106abe28b02dfe586a9896b7d2ad76f94efb755
ruigao <ruigao2@ebay.com>
2015-05-06 17:19:09 +0800

    update upload

--- app/scripts/controllers/admin/video-manger.js
+++ app/scripts/controllers/admin/video-manger.js
@@ -8,5 +8,30 @@
  * Controller of the cimWebappApp
  */
 Site.controller('VideoManagerCtrl', ['$scope', '$http','$filter', '$window', function ($scope, $http, $filter, $window) {
+  var uploadObj = $("#fileUpload").uploadFile({
+    url : "upload.php",
+    allowedTypes : "xml,png",
+    multiple : false,
+    autoSubmit : false,
+    fileName : "file",
+    maxFileSize : 1024 * 1024 * 1,
+    maxFileCount : 1,
+    showProgress : true,
+    showFileCounter : false,
+    showStatusAfterSuccess : true,
+    uploadButtonClass : "ajax-file-upload-blue",
+    onSubmit:function(files) {
+      $("#eventsmessage").html($("#eventsmessage").html()+"<br/>Submitting:"+JSON.stringify(files));
+    },
+    onSuccess:function(files,data,xhr) {
 
-}])
+    },
+    onError: function(files,status,errMsg) {
+      $("#eventsmessage").html($("#eventsmessage").html()+"<br/>Error for: "+JSON.stringify(files));
+    }
+  });
+
+  $("#startUpload").on('click', function() {
+    uploadObj.startUpload();
+  });
+}]);
