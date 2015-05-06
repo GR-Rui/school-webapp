5ca8c1589a0694feeb7139ffabf83e71a321f5e6
ruigao <ruigao2@ebay.com>
2015-05-06 16:30:22 +0800

    Add upload feature

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,12 +13,13 @@ var Site = angular.module('Site', [
   'ngRoute',
   'ngSanitize',
   'ngCookies',
-  'ui.router'
+  'ui.router',
+  'blueimp.fileupload'
 ]);
 
 Site.config(
-  [          '$stateProvider', '$urlRouterProvider',
-    function ($stateProvider, $urlRouterProvider) {
+  [          '$stateProvider', '$urlRouterProvider', '$httpProvider', 'fileUploadProvider',
+    function ($stateProvider, $urlRouterProvider, $httpProvider, fileUploadProvider) {
       "use strict";
 
       $stateProvider
@@ -37,5 +38,26 @@ Site.config(
 
         .state('login', {url: '/login', templateUrl: 'views/login.html', controller: 'LoginCtrl'});
       $urlRouterProvider.otherwise('/admin/SG0822888');
+
+
+      //upload config
+      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+      fileUploadProvider.defaults.redirect = window.location.href.replace(
+        /\/[^\/]*$/,
+        '/cors/result.html?%s'
+      );
+      var isOnGitHub = false;
+      if (isOnGitHub) {
+        // Demo settings:
+        angular.extend(fileUploadProvider.defaults, {
+          // Enable image resizing, except for Android and Opera,
+          // which actually support image resizing, but fail to
+          // send Blob objects via XHR requests:
+          disableImageResize: /Android(?!.*Chrome)|Opera/
+            .test(window.navigator.userAgent),
+          maxFileSize: 5000000,
+          acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+        });
+      }
 
     }]);
